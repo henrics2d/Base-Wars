650bf69a312e7dd5e6b10019a9448e729e2f98aa
henri <kinitocouves@gmail.com>
2022-09-21 20:54:15 +0100

    Added dompoints as spawnpoints

--- spawns.lua
+++ spawns.lua
@@ -1,6 +1,11 @@
 tdm.spawnpoints = {}
 for _,e in pairs(entitylist()) do
   if entity(e.x,e.y,"typename") == "Info_Dom_Point" then
-    tdm.spawnpoints[#tdm.spawnpoints] = e
+    tdm.spawnpoints[#tdm.spawnpoints+1] = e
   end
+end
+
+
+for _,e in ipairs(tdm.spawnpoints) do
+  print(e.x..","..e.y.."; "..entity(e.x,e.y,"name"))
 end
