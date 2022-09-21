3d9245089cedacbd0bbda609c9db075f85cac098
Magnus Gunnarsson <EnderCrypt@gmail.com>
2022-09-22 01:10:33 +0200

    fix bad argument check

--- talents.lua
+++ talents.lua
@@ -60,7 +60,7 @@ function tdm.listPlayersMissingTalents(id)
 end
 
 function tdm.listPlayersTalents(id)
-	checks.requireType("id", id, "string")
+	checks.requireType("id", id, "number")
 	local talents = {}
 	for talent,_ in pairs(tdm.player[id].talents) do
 		talents[#talents + 1] = talent
